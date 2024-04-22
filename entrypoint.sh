fa15faaca950e24fe0ce5e053137e0309ae98801
soyvitou2019 <9ball2019@gmail.com>
2024-04-22 09:52:10 +0700

    add entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+# Change directory to application directory
 cd /var/www
 
+# Check if vendor/autoload.php exists, if not, run composer install
 if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
+# Check if .env file exists, if not, copy from .env.example
 if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
+    echo "Creating .env file"
     cp .env.example .env
-else
-    echo "env file exists."
 fi
 
+# Set container role
 role=${CONTAINER_ROLE:-app}
 
+# If role is "app", execute necessary commands
 if [ "$role" = "app" ]; then
-    php artisan migrate
+    php artisan migrate --force
     php artisan key:generate
     php artisan cache:clear
     php artisan config:clear
     php artisan route:clear
-    php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
-    exec docker-php-entrypoint "$@"
+    php artisan serve --host=0.0.0.0 --port=80
+else
+    # Else, execute whatever other command needed
+    exec "$@"
 fi
